9d369c8beead6a1415a3f58d534a2b8241fff793
mpaulweeks <mpaulweeks@gmail.com>
2018-03-15 09:40:06 -0400

    draft flask server

---
+++ py/server.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import os
+from flask import (
+    Flask,
+    send_file,
+)
+
+EVENT_CACHE = {}
+
+
+def get_cached_event(key):
+    if key not in EVENT_CACHE:
+        if key == 'latest':
+            # todo special case
+            pass
+        else:
+            # todo get event
+            EVENT_CACHE[key] = None
+    return EVENT_CACHE[key]
+
+
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='../static',
+)
+
+
+@app.route('/')
+def index():
+    return send_file('../static/index.html')
+
+
+@app.route('/api/event/latest')
+def get_latest_event():
+    return get_cached_event('latest')
+
+
+@app.route('/api/event/<event_id>')
+def get_event_by_id(event_id):
+    return get_cached_event(event_id)
+
+
+@app.route('/api/scrape')
+def scrape_events():
+    # todo scrape and insert into db
+    EVENT_CACHE.clear()
+
+
+def main():
+    with open('server.pid', 'wt') as f:
+        f.write(str(os.getpid()))
+    app.run(
+        # debug=True,
+        host='localhost',
+        port=6200,
+    )
+
+
+if __name__ == "__main__":
+    main()
